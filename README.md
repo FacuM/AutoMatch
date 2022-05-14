20b409a871c499f12f01ccd593a7af4adeb98756
FacuM <facumo.fm@gmail.com>
2022-05-13 22:51:22 -0300

    Initial release

---
+++ README.md
@@ -0,0 +1,56 @@
+AutoMatch
+==========
+
+**AutoMatch** is a CLI bot for Tinder which will (probably) get you better, faster and more reliable interactions.
+
+# About
+
+This software was designed just for fun, playing around with external APIs and as a learning exercise. I won't provide any kind of pro-level support for this software and you shouldn't expect it to work for long, since Tinder's API could change at any time without prior notice.
+
+For more information about Tinder's API, please check out [this repo](https://github.com/fbessez/Tinder).
+
+# Features
+
+- [X] Forbidden strings list
+
+    Create a file named "forbidden_strings" and type the keywords and/or sentences that would make you feel uninterested about a recommendation, strings are separated by newlines.
+
+- [X] Known profiles list
+
+    The script will keep track of known profiles (any profile it's seen throughout it's lifetime on the current device) and automatically give them a "pass" if they've been seen before. This is a workaround for Tinder's API where it might recommend people you've already liked and they've rejected your like in the past.
+
+- [X] Irrelevant bio blocking
+
+    Detect and automatically skip profiles without a real bio (automatic parsing of bios containing just an Instagram username). Empty bios will also be skipped. 
+
+- [X] Automatic or manual likes
+
+    Toggle ASK_BEFORE_LIKE in order to set whether you want the bot to automatically like suggested profiles that match your criteria or get a selection of choices, using the manual mode also allows you to check your suggested match photos by typing "p".
+
+- [ ] ~~Customizable internal settings (easy access to what's currently modified through constants).~~
+- [ ] ~~Main menu with account settings options (max distance, age range, etc.).~~
+
+# Requirements
+
+- PHP 7.4 or greater
+- cURL extension for PHP
+
+# Usage
+
+- Run the program: `php index.php`, you'll be asked for a token. Continue with the next steps to find it.
+- Sign into **Tinder** with your **desktop computer using a web browser**.
+- Open **dev tools (hit F12)** and **reload the page**.
+- Select the **Network** tab and choose **Fetch/XHR** from the top bar.
+- Pick any request from the list, once the **Headers** window has opened, check the **Request URL**. Make sure it starts with "https://api.gotinder.com". If it doesn't, go through the rest until you find one that matches.
+- Once you've found it, scroll to the bottom of the **Headers** tab and copy the value of the **x-auth-token** header.
+- Paste it in the console and hit enter. That's it, you're good to go!
+
+# Disclaimer
+
+Please take into consideration that I'm not affiliated in any way with the Tinder company. All this code has been written through experimentation and public documentation found online with no intent to cause any damages.
+
+If you feel like this isn't the case, feel free to file a takedown request.
+
+# License
+
+This project is licensed under the terms of [The Unlicense](LICENSE).
---
+++ index.php
@@ -0,0 +1,677 @@
+<?php
+
+define('TOKEN_FILE', 'token');
+define('FORBIDDEN_STRINGS_FILE', 'forbidden_strings');
+define('KNOWN_PROFILES_FILE', 'known_profiles.json');
+
+define('DEBUG', false);
+
+// Progress text max length
+define('PROGRESS_MAX_LENGTH', 80); // characters
+
+define('MILES_IN_KM', 1.60934);
+
+define('ORIGIN_URL',     'https://tinder.com');
+define('BASE_URL',       'api.gotinder.com');
+define('TINDER_VERSION', '3.27.1');
+
+// This is the user agent we're mocking.
+define('USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36');
+
+// How much time to wait between requests if we're out of recommendations.
+define('NO_RECOMMENDATIONS_RETRY_INTERVAL', 5); // minutes
+
+/*
+ * What's the maximum time to wait between requests once we've gone through all
+ * the current set of recommendations.
+ * 
+ * This is a random number between 0 and this value.
+ */
+define('MAX_SLEEP_TIME', 3); // seconds
+
+// Minimum length of a bio to be considered relevant.
+define('BIO_MIN_LENGTH', 200); // characters
+
+define('AUTO_LIKE',       true);
+define('ASK_BEFORE_LIKE', true);
+define('ASK_BEFORE_LIKE_DEFAULT_CHOICE', 'n'); // 'y' or 'n'
+define('AUTO_PASS',       true);
+
+define('DISTANCE_TO_KM', true);
+
+/**
+ * request
+ * 
+ * Make a request to the Tinder API.
+ *
+ * @param  string       $url        URL to request
+ * @param  string       $method     HTTP method to use
+ * @param  array|null   $data       Data to send
+ * 
+ * @return array|null
+ */
+function request($url, $method = 'GET', $data = null) {
+    $ch = curl_init();
+
+    $url = 'https://' . BASE_URL . $url;
+
+    if ($data && $method == 'GET') {
+        $url .= '?' . http_build_query($data);
+    }
+
+    curl_setopt($ch, CURLOPT_URL,            $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  $method);
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER,     getHeaders());
+
+    if ($data && $method != 'GET') {
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
+
+    $result = curl_exec($ch);
+
+    if (curl_errno($ch) || curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200) {
+        throw new Exception('An unxpected error has occurred: ' . curl_error($ch) . ' - ' . $result);
+    }
+
+    curl_close($ch);
+
+    $result = json_decode($result, true);
+
+    if (
+        $result
+        &&
+        isset($result['data']) && !empty($result['data'])
+    ) {
+        return $result['data'];
+    }
+
+    return null;
+}
+
+/**
+ * requestAuthToken
+ * 
+ * Request an auth token for the Tinder API (must be provided by the user
+ * from the browser).
+ *
+ * @param  bool $fromCrash  Whether this is being called from a crash
+ * 
+ * @return void
+ */
+function requestAuthToken($fromCrash = false) {
+    global $authToken;
+
+    print (
+        $fromCrash
+            ? 'Something went wrong.' . PHP_EOL .
+              'Please try logging in again in a browser and typing a new token (press CTRL-C to abort): '
+            : 'Please enter your Tinder auth token: '
+    );
+
+    $authToken = '';
+
+    while (empty($authToken)) {
+        $handle = fopen("php://stdin", "r");
+
+        $authToken = trim(fgets($handle));
+
+        if (!empty($authToken)) {
+            file_put_contents(TOKEN_FILE, $authToken);
+        }
+
+        fclose($handle);
+
+        if (empty($authToken)) {
+            print 'You must supply a token, please try again: ';
+        }
+    }
+}
+
+/**
+ * getSleepTime
+ * 
+ * Get the time to sleep between requests.
+ * 
+ * Returns a random number between 0 and MAX_SLEEP_TIME.
+ *
+ * @return int
+ */
+function getSleepTime() {
+    return rand(0, MAX_SLEEP_TIME);
+}
+
+function removeTildes($string) {
+    $string = str_replace(['á', 'à', 'ä', 'â', 'ã', 'ª'], 'a', $string);
+    $string = str_replace(['Á', 'À', 'Â', 'Ä'], 'A', $string);
+    $string = str_replace(['é', 'è', 'ë', 'ê'], 'e', $string);
+    $string = str_replace(['É', 'È', 'Ê', 'Ë'], 'E', $string);
+    $string = str_replace(['í', 'ì', 'ï', 'î'], 'i', $string);
+    $string = str_replace(['Í', 'Ì', 'Ï', 'Î'], 'I', $string);
+    $string = str_replace(['ó', 'ò', 'ö', 'ô', 'õ', 'º'], 'o', $string);
+    $string = str_replace(['Ó', 'Ò', 'Ö', 'Ô', 'Õ'], 'O', $string);
+    $string = str_replace(['ú', 'ù', 'ü', 'û'], 'u', $string);
+    $string = str_replace(['Ú', 'Ù', 'Û', 'Ü'], 'U', $string);
+    $string = str_replace(['ý', 'ÿ'], 'y', $string);
+    $string = str_replace(['Ý'], 'Y', $string);
+
+    return $string;
+}
+
+function removeEmojis($string) {
+    $string = preg_replace('/[\x{1F300}-\x{1F64F}]/u', '', $string);
+    $string = preg_replace('/[\x{1F680}-\x{1F6FF}]/u', '', $string);
+    $string = preg_replace('/[\x{2600}-\x{26FF}]/u', '', $string);
+    $string = preg_replace('/[\x{2700}-\x{27BF}]/u', '', $string);
+    $string = preg_replace('/[\x{1F600}-\x{1F64F}]/u', '', $string);
+
+    return $string;
+}
+
+function removeLineBreaks($string) {
+    return str_replace([ "\r", "\n" ], ' ', $string);    
+}
+
+/**
+ * parseForbiddenStrings
+ *
+ * Parse the forbidden strings file and builds an array of forbidden strings
+ * on top of the $forbiddenStrings global variable.
+ *
+ * Both the original strings and the version without tildes are added to the
+ * array.
+ *  
+ * @return void
+ */
+function parseForbiddenStrings() {
+    global $forbiddenStrings;
+
+    if (!file_exists(FORBIDDEN_STRINGS_FILE)) {
+        file_put_contents(FORBIDDEN_STRINGS_FILE, '');
+    }
+
+    $forbiddenStrings = [];
+
+    foreach ( explode(PHP_EOL, file_get_contents(FORBIDDEN_STRINGS_FILE)) as $string ) {
+        $string = trim($string);
+
+        if (empty($string)) { continue; }
+
+        if (!in_array($string, $forbiddenStrings)) {
+            $forbiddenStrings[] = $string;
+        }
+
+        $withoutTildes = removeTildes($string);
+
+        if (!in_array($withoutTildes, $forbiddenStrings)) {
+            $forbiddenStrings[] = $withoutTildes;
+        }
+    }
+}
+
+/**
+ * getHeaders
+ * 
+ * Get the headers of the API.
+ * 
+ * @param  bool $json  Whether to pass the required content-type header
+ *
+ * @return array
+ */
+function getHeaders($json = false) {
+    global $authToken;
+
+    $headers = [
+        'Authority: ' . BASE_URL,
+        'Accept: application/json',
+        'Origin: '  . ORIGIN_URL,
+        'Referer: ' . ORIGIN_URL . '/',
+        'Tinder-Version: ' . TINDER_VERSION,
+        'User-Agent: ' . USER_AGENT,
+        'X-Auth-Token: ' . $authToken,
+        'X-Supported-Image-Formats: webp,jpeg'
+    ];
+
+    if ($json) {
+        $headers[] = 'Content-Type: application/json';
+    }
+
+    return $headers;
+}
+
+/**
+ * getUser
+ * 
+ * Get the user data.
+ *
+ * @return array|null
+ */
+function getUser() {
+    try {
+        return request('/v2/profile', 'GET', [ 'include' => 'user' ]);
+    } catch (Exception $e) {
+        print 'Failed to fetch profile data: ' . $e->getMessage() . PHP_EOL;
+    }
+
+    return null;
+}
+
+/**
+ * getRecs
+ * 
+ * Get the recommendations from Tinder's API.
+ *
+ * @return array
+ */
+function getRecs() {
+    try {
+        return request('/v2/recs/core', 'GET');
+    } catch (Exception $e) {
+        print 'Failed to fetch recommendations: ' . $e->getMessage() . PHP_EOL;
+
+        exit(1);
+    }
+}
+
+/**
+ * storeKnownProfile
+ *
+ * Store a profile in the known profiles file.
+ * 
+ * @param  string $userId
+ * 
+ * @return void
+ */
+function storeKnownProfile($userId) {
+    global $knownProfiles;
+
+    if (!in_array($userId, $knownProfiles)) {
+        $knownProfiles[] = $userId;
+    }
+
+    file_put_contents(KNOWN_PROFILES_FILE, json_encode($knownProfiles));
+}
+
+/**
+ * readKnownProfiles
+ * 
+ * Read the known profiles from the file.
+ *
+ * @return bool True if the file was read, false otherwise.
+ */
+function readKnownProfiles() {
+    global $knownProfiles;
+
+    $knownProfiles = [];
+
+    if (file_exists(KNOWN_PROFILES_FILE)) {
+        try {
+            $knownProfiles = json_decode(
+                file_get_contents(KNOWN_PROFILES_FILE), // file
+                true,                                   // assoc
+                512,                                    // depth
+                JSON_THROW_ON_ERROR                     // options
+            );
+        } catch (Exception $e) {
+            print
+                'Failed to decode known profiles: ' . $e->getMessage() . PHP_EOL .
+                PHP_EOL .
+                'The file will be deleted and a new one will be created.' . PHP_EOL;
+
+            file_put_contents(KNOWN_PROFILES_FILE, json_encode($knownProfiles));
+
+            return false;
+        }
+    } else {
+        file_put_contents(KNOWN_PROFILES_FILE, json_encode($knownProfiles));
+    }
+
+    return true;
+}
+
+/**
+ * pass
+ *
+ * @param  string $userId
+ * 
+ * @return void
+ */
+function pass($userId) {
+    if (DEBUG) {
+        print 'Passing ' . $userId . '...' . PHP_EOL;
+    }
+
+    storeKnownProfile($userId);
+
+    try {
+        request('/pass/' . $userId, 'GET');
+    } catch (Exception $e) {
+        print 'Failed to pass ' . $userId . ': ' . $e->getMessage() . PHP_EOL;
+
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * like
+ *
+ * @param  string $userId
+ * 
+ * @return void
+ */
+function like($userId) {
+    if (DEBUG) {
+        print 'Liking ' . $userId . '...' . PHP_EOL;
+    }
+
+    storeKnownProfile($userId);
+
+    try {
+        request('/like/' . $userId, 'POST');
+    } catch (Exception $e) {
+        print 'Failed to like ' . $userId . ': ' . $e->getMessage() . PHP_EOL;
+
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * bioHasRelevantText
+ *
+ * This method checks if the bio has relevant text. If the bio contains just an
+ * Instagram username, it is not relevant.
+ * 
+ * If there's no bio, it is automatically irrelevant.
+ * 
+ * @param  mixed $bio
+ * 
+ * @return bool
+ */
+function bioHasRelevantText($bio) {
+    global $irrelevantReason;
+
+    $bio = strtolower($bio);
+
+    // Try to remove the Instagram username
+    $bio = preg_replace('/ig:.*[ ]/', '', strtolower($bio));
+    $bio = preg_replace('/@.*[ ]/', '', $bio);
+
+    $bio = trim($bio);
+
+    if (empty($bio)) {
+        $irrelevantReason = 'no text after removing Instagram username';
+
+        return false;
+    }
+
+    if (strlen($bio) < BIO_MIN_LENGTH)  {
+        $irrelevantReason = 'bio is too short';
+
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * bioHasForbiddenStrings
+ *
+ * This method checks if the bio has banned substrings.
+ * 
+ * @param  string $bio
+ * 
+ * @return bool
+ */
+function bioHasForbiddenStrings($bio) {
+    global $forbiddenStrings;
+
+    $bio = removeTildes(
+        strtolower($bio)
+    );
+
+    foreach ($forbiddenStrings as $string) {
+        if (strpos($bio, $string) !== false) { return true; }
+    }
+}
+
+$irrelevantReason = null;
+
+if (file_exists(TOKEN_FILE)) {
+    $authToken = trim( file_get_contents(TOKEN_FILE) );
+
+    if (empty($authToken)) { requestAuthToken(); }
+} else {
+    requestAuthToken();
+}
+
+print 'Parsing known profiles... ';
+if (readKnownProfiles()) {
+    print 'OK!' . PHP_EOL;
+}
+
+print 'Parsing forbidden strings... ';
+parseForbiddenStrings();
+print 'OK!' . PHP_EOL;
+
+$user = null;
+
+while (!$user) {
+    print 'Getting user data... ';
+    $user = getUser();
+
+    if (!$user) {
+        requestAuthToken(true);
+
+        continue;
+    }
+
+    print 'OK!' . PHP_EOL;
+}
+
+print 'Getting recommendations... ';
+$result = getRecs();
+print 'OK!' . PHP_EOL;
+
+print
+    PHP_EOL .
+    'Welcome ' . $user['user']['name'] . '!' . PHP_EOL .
+    PHP_EOL;
+
+$maxDistance = $user['user']['distance_filter'];
+
+if (DISTANCE_TO_KM) {
+    $maxDistance *= MILES_IN_KM;
+}
+
+print
+    'Forbidden strings: ' . ($forbiddenStrings ? implode(', ', $forbiddenStrings) : '(none)') . PHP_EOL .
+    'Age range: between ' . $user['user']['age_filter_min']  . ' and ' . $user['user']['age_filter_max'] . ' years.' . PHP_EOL .
+    'Max distance: '      . $maxDistance . ' ' . (DISTANCE_TO_KM ? 'km' : 'mi') . '.' . PHP_EOL .
+    PHP_EOL;
+
+while (true) {
+    while ($result && isset($result['results'])) {
+        $recsCount = count($result['results']);
+
+        foreach ($result['results'] as $index => $person) {
+            $irrelevantReason = null;
+
+            $photos = array_map(function ($photo) {
+                return $photo['url'];
+            }, $person['user']['photos']);
+
+            if (DEBUG) {
+                print
+                    '==== SESSION START ===='              . PHP_EOL .
+                    'Person: ' . $person['user']['name']   . PHP_EOL .
+                    'Bio: '    . $person['user']['bio']    . PHP_EOL;
+            }
+
+            $bioHasRelevantText     = bioHasRelevantText($person['user']['bio']);
+            $bioHasForbiddenStrings = bioHasForbiddenStrings($person['user']['bio']);
+
+            if (DEBUG) {
+                print '==== SESSION END ====' . PHP_EOL;
+            }
+
+            $age = null;
+
+            if (isset($person['user']['birth_date'])) {
+                $age = (new \DateTime())->diff(new \DateTime($person['user']['birth_date']))->y;
+            }
+
+            $distance = $person['distance_mi'];
+
+            if (DISTANCE_TO_KM) {
+                $distance *= MILES_IN_KM;
+            }
+
+            $distance = round($distance, 2);
+
+            $message =
+                (DEBUG ? '' : "\r") . '[ ' . ($index + 1) . '/' . $recsCount . ' ] ' . // [ #/# ]
+                $person['user']['name'] . ' (' . ($age ? $age . 'y, ': '') . $distance . ' ' . (DISTANCE_TO_KM ? 'km' : 'mi') . '.): ';   // Name (age, distance)
+
+            $message .= substr(
+                removeLineBreaks( removeEmojis( $person['user']['bio'] ) ),  // input
+                0,                                                           // start
+                PROGRESS_MAX_LENGTH - strlen($message) - 1                   // length
+            );
+            
+            print $message;
+
+            if (!DEBUG) {
+                // Clean the overriden line
+                for ($i = 0; $i <= PROGRESS_MAX_LENGTH; $i++) {
+                    print ' ';
+                }
+            }
+
+            if (!$bioHasRelevantText || $bioHasForbiddenStrings) {
+                if (!$bioHasRelevantText && DEBUG) {
+                    print 'Bio has no relevant text (' . $irrelevantReason . '), skipping.' . PHP_EOL;
+                }
+
+                if ($bioHasForbiddenStrings && DEBUG) {
+                    print 'Bio has forbidden strings, skipping.' . PHP_EOL;
+                }
+
+                if (AUTO_PASS) {
+                    pass($person['user']['_id']);
+                }
+
+                continue;
+            }
+
+            if (!DEBUG) { print PHP_EOL; }
+
+            print
+                '===> ' . (AUTO_LIKE && !ASK_BEFORE_LIKE ? 'Liked' : 'Suggested') .
+                ' person: ' . $person['user']['name'] . ' (' . $age . 'y, ' . $distance . ' ' . (DISTANCE_TO_KM ? 'km' : 'mi') . '.)' .
+                ' with ID ' . $person['user']['_id'] . ',' .
+                ' their bio says: ' . PHP_EOL . $person['user']['bio'] .
+                PHP_EOL;
+
+            if (AUTO_LIKE) {
+                if (in_array($person['user']['_id'], $knownProfiles)) {
+                    print 'API BUG: Already liked this person, forcing pass... ';
+
+                    pass($person['user']['_id']);
+
+                    print 'OK!' . PHP_EOL;
+
+                    continue;
+                }
+
+                if (!ASK_BEFORE_LIKE) {
+                    like($person['user']['_id']);
+                } else {
+                    while (
+                        !isset($line)
+                        ||
+                        (
+                            trim( strtolower($line) ) !== 'y'
+                            &&
+                            trim( strtolower($line) ) !== 'n'
+                        )
+                    ) {
+                        print
+                            'Do you like ' . $person['user']['name'] . '? (Yy|Nn|Pp = view photos) ['
+                                . strtoupper(ASK_BEFORE_LIKE_DEFAULT_CHOICE) . strtolower(ASK_BEFORE_LIKE_DEFAULT_CHOICE) .
+                            ']: ';
+
+                        $handle = fopen('php://stdin', 'r');
+
+                        $line = trim(
+                            strtolower( fgets($handle) )
+                        );
+
+                        fclose($handle);
+
+                        if (empty($line)) { $line = ASK_BEFORE_LIKE_DEFAULT_CHOICE; }
+
+                        if ($line === 'y') {
+                            like($person['user']['_id']);
+                        } else if ($line === 'n') {
+                            pass($person['user']['_id']);
+                        } else if ($line === 'p') {
+                            print 'Photos:' . PHP_EOL;
+
+                            foreach ($photos as $photo) {
+                                print ' - ' . $photo . PHP_EOL;
+                            }
+                        } else if (
+                            $line !== 'y'
+                            &&
+                            $line !== 'n'
+                            &&
+                            $line !== 'p'
+                        ) {
+                            print 'Invalid choice, try again.' . PHP_EOL;
+                        }
+                    }
+
+                    if (isset($line)) {
+                        unset($line);
+                    }
+                }
+            }
+        }
+
+        if (!DEBUG) { print PHP_EOL; }
+
+        for ($i = getSleepTime( ); $i > 0; $i--) {
+            print "\r" . 'Sleeping for ' . $i . ' second' . ($i == 1 ? '' : 's') . '... ';
+
+            sleep(1);
+        }
+
+        print PHP_EOL;
+
+        print 'Waiting for next batch... ';
+        $result = getRecs();
+        print 'OK!' . PHP_EOL;
+    }
+
+    print 'Ran out of recommendations, try increasing the distance filter.' . PHP_EOL;
+
+    for ($remainingTime = (NO_RECOMMENDATIONS_RETRY_INTERVAL * 60); $remainingTime > 0; $remainingTime--) {
+        print "\r" . 'Retrying in ' . $remainingTime . ' seconds... ';
+
+        sleep(1);
+    }
+
+    print PHP_EOL;
+
+    print 'Getting recommendations... ';
+    $result = getRecs();
+    print 'OK!' . PHP_EOL;
+}
+
+?>
